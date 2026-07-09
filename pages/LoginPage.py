497565224b33f61456d15d9066b2ed9ee1e67fbf
Rovshan97 <imehraliev@gmail.com>
2026-07-09 11:01:18 +0300

    Поправил локаторы

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -9,12 +9,16 @@ class LoginPageLocators:
     LOGIN_BUTTON = (By.XPATH, '//*[@data-test-id="enter-action"]')
     LOGIN_BY_QR = (By.XPATH, '//*[@label="Войти по QR-коду"]')
     HIDE_PASSWORD = (By.XPATH, '//*[@aria-label="Не получается войти?"]')
-    REGISTRATION = (By.XPATH, '//div[@class="LoginFormMain-module__bottom___YLtCo"]/button[@type="button"]')
+    REGISTRATION = (By.XPATH, '//*[text()="Зарегистрироваться"]')
     AUTHORIZATION_BY_VK = (By.XPATH, '//*[@data-l="t,vkc"]')
     AUTHORIZATION_BY_MAIL_RU = (By.XPATH, '//*[@data-l="t,mailru"]')
     AUTHORIZATION_BY_YANDEX = (By.XPATH, '//*[@data-l="t,yandex"]')
     OTHER_BUTTON = (By.XPATH, '//*[@data-l="t,other"]')
-    ERROR_TEXT = (By.XPATH, '//*[@class="LoginForm-module__error___1xmAD vkuiCaption__sizeYNone vkuiCaption__level1 vkuiTypography__host vkuiTypography__normalize vkuiRootComponent__host"]')
+    ERROR_TEXT = (By.XPATH, '//*[text()="Введите пароль"]')
+    RECOVERY_LINK(By.XPATH, '//*[text()="Восстановить"]')
+    CANCEL_LINK(By.XPATH, '//*[text()="Отмена"]')
+
+
 
 class LoginPageHelper(BasePage):
     def __init__(self, driver):
