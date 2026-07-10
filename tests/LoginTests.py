04ce0a7697c0905fc7fc0d2c4dd1b0c880e94a55
Rovshan97 <imehraliev@gmail.com>
2026-07-10 11:22:42 +0300

    Повторил код из урока

--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -3,9 +3,11 @@ from pages.BasePage import BasePage
 from pages.LoginPage import LoginPageHelper
 import allure
 
-BASE_URL = "https://ok.ru/"
-EMPTY_LOGIN_ERROR = "Введите логин"
-EMPTY_PASSWORD_ERROR = "Введите пароль"
+BASE_URL = "https://sn.rv-school.ru/"
+EMPTY_LOGIN_ERROR = "Введите телефон, email или логин и пароль."
+EMPTY_PASSWORD_ERROR = "Введите телефон, email или логин и пароль."
+LOGIN = "admin"
+PASSWORD = "12"
 
 
 @allure.suite("Проверка формы авторизации")
@@ -22,6 +24,6 @@ def test_empty_login_and_password(browser):
 def test_empty_password_error(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
-    LoginPage.enter_login("admin")
+    LoginPage.enter_login(LOGIN)
     LoginPage.click_login()
     assert LoginPage.get_text_error() == EMPTY_PASSWORD_ERROR
